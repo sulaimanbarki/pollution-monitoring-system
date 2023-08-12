96e43031c709e9132386d7a7d88ff2586b086e20
Sulaiman Barki <sulaimanbarki@gmail.com>
2023-08-12 17:28:16 +0500

    package.json file updated

--- config/baseUrl.js
+++ config/baseUrl.js
@@ -1,9 +1,4 @@
 const path = require("path");
-const baseUrl = __dirname;
+
+const baseUrl = path.dirname(require.main.filename);
 module.exports = baseUrl;
-
-
-// const path = require("path");
-
-// const baseUrl = path.dirname(require.main.filename);
-// module.exports = baseUrl;
